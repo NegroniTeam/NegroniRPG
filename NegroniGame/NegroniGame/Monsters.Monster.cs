a9ce680e1957a2e3e55a46ca4ba2ed11c5c1099f
IvanChardakov <ivan.chardakov@gmail.com>
2014-02-13 13:54:37 +0200

    Enemy movement

--- NegroniGame/NegroniGame/Monsters.Monster.cs
+++ NegroniGame/NegroniGame/Monsters.Monster.cs
@@ -11,6 +11,9 @@
     public class Monster : Interfaces.IMonster
     {
         private const int HP_POINTS_INITIAL = 100;
+        private const float TIME_TO_CHANGE_DIRECTION = 200f;
+
+        public Rectangle MonsterPosition;
 
         public Monster(List<Texture2D> mobTextures, Rectangle spawnPosition)
         {
@@ -25,22 +28,63 @@
         public void Move(GameTime gameTime)
         {
             // some logic for movement
+            Random randomGenerator = new Random();
+            int direction = -1;
 
-            //int direction = randomGenerator.Next(1, 4);
-            //int positions = randomGenerator.Next(10, 400);
+            this.ElapsedChangeMonsterPosition += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
 
-            //if (direction == 1)
-            //{
-            //    // right
-            //}
+            if (this.ElapsedChangeMonsterPosition >= TIME_TO_CHANGE_DIRECTION)
+            {
+                direction = randomGenerator.Next(1, 5);
+                this.ElapsedChangeMonsterPosition = 0;
+            }
 
-            // this.MonsterPosition.X += 2f;
-            // this.MonsterPosition.Y += 2f;
-            // this.MonsterAnim = this.MonsterTextures[0];
+            if (direction == 1)// right
+            {
+                if (this.MonsterPosition.X < Screens.GameScreen.ScreenWidth - 30)
+                {
+                    this.MonsterPosition.X += 10;
+                    this.SourcePosition = Animate(gameTime);
+                    this.MonsterAnim = this.MonsterTextures[0];
+                    this.Direction = SystemFunctions.DirectionsEnum.East;
+                }
+            }
+
+            else if (direction == 2)// left
+            {
+                if (this.MonsterPosition.X > 0)
+                {
+                    this.MonsterPosition.X -= 10;
+                    this.SourcePosition = Animate(gameTime);
+                    this.MonsterAnim = this.MonsterTextures[1];
+                    this.Direction = SystemFunctions.DirectionsEnum.West;
+                }
+            }
+
+            else if (direction == 3)// top
+            {
+                if (this.MonsterPosition.Y > 0)
+                {
+                    this.MonsterPosition.Y -= 10;
+                    this.SourcePosition = Animate(gameTime);
+                    this.MonsterAnim = this.MonsterTextures[2];
+                    this.Direction = SystemFunctions.DirectionsEnum.North;
+                }
+            }
+
+            else if (direction == 4) // bottom
+            {
+                if (this.MonsterPosition.Y <= Screens.GameScreen.ScreenHeight - 170)
+                {
+                    this.MonsterPosition.Y += 10;
+                    this.SourcePosition = Animate(gameTime);
+                    this.MonsterAnim = this.MonsterTextures[3];
+                    this.Direction = SystemFunctions.DirectionsEnum.South;
+                }
+            }
+
             this.SourcePosition = Animate(gameTime);
-
-            //this.SourcePosition = new Rectangle(64, 0, 64, 64);
-            // this.DestinationPosition = new Rectangle((int)this.MonsterPosition.X, (int)this.MonsterPosition.Y, 64, 64);
+            this.DestinationPosition = new Rectangle((int)this.MonsterPosition.X, (int)this.MonsterPosition.Y, 32, 32);
 
         }
         private Rectangle Animate(GameTime gameTime)
@@ -66,21 +110,20 @@
 
         public void Draw(SpriteBatch sb)
         {
-            new SystemFunctions.Sprite(this.MonsterAnim, this.MonsterPosition, this.SourcePosition).DrawBoxAnim(sb);
+            new SystemFunctions.Sprite(this.MonsterAnim, this.DestinationPosition, this.SourcePosition).DrawBoxAnim(sb);
         }
 
-        public Rectangle MonsterPosition { get; private set; }
         public string Name { get; private set; }
         public List<Texture2D> MonsterTextures { get; private set; }
         public float Elapsed { get; private set; }
+        public float ElapsedChangeMonsterPosition { get; private set; }
         public float Delay { get; private set; }
         public int Frames { get; private set; }
         public Texture2D MonsterAnim { get; private set; }
         public Rectangle SourcePosition { get; private set; }
         public int HpPointsCurrent { get; set; }
+        public SystemFunctions.DirectionsEnum Direction { get; private set; }
 
-        // public Rectangle DestinationPosition { get; private set; }
-
-        // public Vector2  MonsterPosition { get; private set; } <- TO DO
+        public Rectangle DestinationPosition { get; private set; }
     }
 }
